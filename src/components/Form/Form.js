a2c027c5aa146edfb9a783cae4bc613e0127e9ba
MDABDULLAH16 <mdabdullah161036@gmail.com>
2023-01-05 18:54:59 +0600

    email form ui setup with onBlur

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -4,12 +4,27 @@ import Form from "react-bootstrap/Form";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 const Forms = () => {
+  const handleSubmit = (event) => {
+    event.preventDefault();
+  };
+
+  const handleEmailBlur = (event) => {
+    console.log(event.target.value);
+  };
+  const handlePasswordBlur = (event) => {
+    console.log(event.target.value);
+  };
   return (
-    <div className=" w-50 mx-auto mb-4">
-      <Form>
+    <div className=" w-50 mx-auto mt-4 mb-4">
+      <h2 className="text-primary">Please Register First!!</h2>
+      <Form onSubmit={handleSubmit}>
         <Form.Group className="mb-3" controlId="formBasicEmail">
           <Form.Label>Email address</Form.Label>
-          <Form.Control type="email" placeholder="Enter email" />
+          <Form.Control
+            onBlur={handleEmailBlur}
+            type="email"
+            placeholder="Enter email"
+          />
           <Form.Text className="text-muted">
             We'll never share your email with anyone else.
           </Form.Text>
@@ -17,11 +32,15 @@ const Forms = () => {
 
         <Form.Group className="mb-3" controlId="formBasicPassword">
           <Form.Label>Password</Form.Label>
-          <Form.Control type="password" placeholder="Password" />
+          <Form.Control
+            onBlur={handlePasswordBlur}
+            type="password"
+            placeholder="Password"
+          />
         </Form.Group>
-        <Form.Group className="mb-3" controlId="formBasicCheckbox">
+        {/* <Form.Group className="mb-3" controlId="formBasicCheckbox">
           <Form.Check type="checkbox" label="Check me out" />
-        </Form.Group>
+        </Form.Group> */}
         <Button variant="primary" type="submit">
           Submit
         </Button>
