9f1771059a56c1b86674bfaa2361790acfa0e1a2
MDABDULLAH16 <mdabdullah161036@gmail.com>
2023-01-11 20:16:19 +0600

    form validation

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,30 +1,66 @@
-import React from "react";
+import React, { useState } from "react";
 import { Button } from "react-bootstrap";
 import Form from "react-bootstrap/Form";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { createUserWithEmailAndPassword, getAuth } from "firebase/auth";
+import app from "../../firebase.init";
+
+const auth = getAuth(app);
 
 const Forms = () => {
-  const handleSubmit = (event) => {
-    event.preventDefault();
-  };
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+  const [validated, setValidated] = useState(false);
 
   const handleEmailBlur = (event) => {
-    console.log(event.target.value);
+    setEmail(event.target.value);
   };
   const handlePasswordBlur = (event) => {
-    console.log(event.target.value);
+    setPassword(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    //form validity
+    const form = event.currentTarget;
+    if (form.checkValidity() === false) {
+      event.stopPropagation();
+      return;
+    }
+    if (!/(?=.*[a-zA-Z >>!#$%&? "<<])[a-zA-Z0-9 >>!#$%&?<< ]/.test(password)) {
+      setError("Please enter a special character");
+      return;
+    }
+
+    setValidated(true);
+    setError();
+    //form validity
+
+    createUserWithEmailAndPassword(auth, email, password)
+      .then((result) => {
+        console.log(result.user);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+    event.preventDefault();
   };
   return (
     <div className=" w-50 mx-auto mt-4 mb-4">
       <h2 className="text-primary">Please Register First!!</h2>
-      <Form onSubmit={handleSubmit}>
+      <Form noValidate validated={validated} onSubmit={handleSubmit}>
         <Form.Group className="mb-3" controlId="formBasicEmail">
           <Form.Label>Email address</Form.Label>
           <Form.Control
             onBlur={handleEmailBlur}
             type="email"
             placeholder="Enter email"
+            required
           />
+          <Form.Control.Feedback type="invalid">
+            Please provide a valid Email.
+          </Form.Control.Feedback>
           <Form.Text className="text-muted">
             We'll never share your email with anyone else.
           </Form.Text>
@@ -36,11 +72,16 @@ const Forms = () => {
             onBlur={handlePasswordBlur}
             type="password"
             placeholder="Password"
+            required
           />
+          <Form.Control.Feedback type="invalid">
+            Please provide a valid Password.
+          </Form.Control.Feedback>
         </Form.Group>
         {/* <Form.Group className="mb-3" controlId="formBasicCheckbox">
           <Form.Check type="checkbox" label="Check me out" />
         </Form.Group> */}
+        <p className="text-danger">{error}</p>
         <Button variant="primary" type="submit">
           Submit
         </Button>
